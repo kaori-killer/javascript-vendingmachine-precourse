559d6e6401da17ac8e8da751b5c84a25b15776c1
kaori-killer <sksbthwjd@daum.net>
2022-08-04 16:27:19 +0900

    feat: [공통] 상단에 탭 메뉴 생성 및 메뉴에 따라 기능 전환

---
+++ src/changeTab.js
@@ -0,0 +1,31 @@
+const productAddMenu = document.getElementById("product-add-menu");
+const vendingMachineManageMenu = document.getElementById("vending-machine-manage-menu");
+const productPurchaseMenu = document.getElementById("product-purchase-menu");
+
+const productAdd = document.getElementById("product-add");
+const vendingMachineManage = document.getElementById("vending-machine-manage");
+const productPurchase = document.getElementById("product-purchase");
+
+const MenuToTab = {
+    "product-add-menu": productAdd,
+    "vending-machine-manage-menu": vendingMachineManage,
+    "product-purchase-menu": productPurchase
+};
+
+const HIDDEN_CLASSNAME = "hidden";
+
+let preTab = productAdd
+let nowTab = productAdd
+
+function onChangeTab(event){
+    event.preventDefault();
+    preTab.classList.add(HIDDEN_CLASSNAME);
+    nowTab = MenuToTab[event.target.id];
+    nowTab.classList.remove(HIDDEN_CLASSNAME);       
+    preTab = nowTab;
+}
+
+
+productAddMenu.addEventListener("click", onChangeTab);
+vendingMachineManageMenu.addEventListener("click", onChangeTab);
+productPurchaseMenu.addEventListener("click", onChangeTab);
