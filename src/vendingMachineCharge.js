0705667d83ac6055a9cc8dba540686d6d7d6936f
kaori-killer <sksbthwjd@daum.net>
2022-08-05 16:21:40 +0900

    feat: [자판기 동전 충전하기] localStorage로 데이터 관리

--- src/vendingMachineCharge.js
+++ src/vendingMachineCharge.js
@@ -1,11 +1,29 @@
 const vendingMachineChargeForm = document.getElementById("vending-machine-charge-form");
 const vendingMachineChargeInput = document.getElementById("vending-machine-charge-input");
+const vendingMachineChargeAmount = document.getElementById("vending-machine-charge-amount");
 
 let money = 0;
+const MONEY_KEY = "money";
+
+function saveMoney(){
+    localStorage.setItem(MONEY_KEY, money);
+}
+
+function paintMoney(){
+    vendingMachineChargeAmount.innerHTML = `보유 금액: ${money}`;
+}
 
 function handleMoneySubmit(event){
     event.preventDefault();
     money += parseInt(vendingMachineChargeInput.value);
+    paintMoney();
+    saveMoney();
 }
+
+const savedMoney = localStorage.getItem(MONEY_KEY);
+if (savedMoney !== null){
+    money = parseInt(savedMoney);
+    paintMoney();
+} 
 
 vendingMachineChargeForm.addEventListener("submit", handleMoneySubmit);
